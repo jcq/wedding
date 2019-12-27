5ed8093da2c9dc81a5faec3fa32d68cece6d6777
JC Quirin <jcq@jcq.local>
2019-12-27 17:07:28 -0500

    Updated header to use bootstrap

---
+++ src/components/Header.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import logo from '../img/logo.svg';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+
+const Header = class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+      navBarActiveClass: ''
+    };
+  }
+
+  toggleHamburger = () => {
+    // toggle the active boolean in the state
+    this.setState(
+      {
+        active: !this.state.active
+      },
+      // after state has been updated,
+      () => {
+        // set the class in state for the navbar accordingly
+        this.state.active
+          ? this.setState({
+              navBarActiveClass: 'is-active'
+            })
+          : this.setState({
+              navBarActiveClass: ''
+            });
+      }
+    );
+  };
+
+  render() {
+    return (
+      <Navbar>
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link as={Link} to="/about">
+                About Us
+              </Nav.Link>
+              <Nav.Link as={Link} to="/events">
+                Events
+              </Nav.Link>
+              <Nav.Link as={Link} to="/get-involved">
+                Get Involved
+              </Nav.Link>
+              <Nav.Link as={Link} to="/rsvp">
+                RSVP
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      // <nav
+      //   className="navbar is-transparent"
+      //   role="navigation"
+      //   aria-label="main-navigation"
+      // >
+      //   <div className="container">
+      //     <div className="navbar-brand">
+      //       <Link to="/" className="navbar-item" title="Logo">
+      //         <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+      //       </Link>
+      //       {/* Hamburger menu */}
+      //       <div
+      //         className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+      //         data-target="navMenu"
+      //         onClick={() => this.toggleHamburger()}
+      //       >
+      //         <span />
+      //         <span />
+      //         <span />
+      //       </div>
+      //     </div>
+      //     <div
+      //       id="navMenu"
+      //       className={`navbar-menu ${this.state.navBarActiveClass}`}
+      //     >
+      //       <div className="navbar-start has-text-centered">
+      //         <Link className="navbar-item" to="/about">
+      //           About Us
+      //         </Link>
+      //         <Link className="navbar-item" to="/events">
+      //           Events
+      //         </Link>
+      //         <Link className="navbar-item" to="/get-involved">
+      //           Get Involved
+      //         </Link>
+      //         <Link className="navbar-item" to="/rsvp">
+      //           RSVP
+      //         </Link>
+      //         <Link className="navbar-item" to="/contact">
+      //           Contact
+      //         </Link>
+      //         <Link className="navbar-item" to="/contact/examples">
+      //           Form Examples
+      //         </Link>
+      //       </div>
+      //     </div>
+      //   </div>
+      // </nav>
+    );
+  }
+};
+
+export default Header;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
+import Header from './Header'
 import './all.scss';
 import useSiteMetadata from './SiteMetadata'
 import { withPrefix } from 'gatsby'
@@ -48,7 +48,7 @@ const TemplateWrapper = ({ children }) => {
           content={`${withPrefix('/')}img/og-image.jpg`}
         />
       </Helmet>
-      <Navbar />
+      <Header />
       <div>{children}</div>
       <Footer />
     </div>
