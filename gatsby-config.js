5a7ed2c7e9f8ce3d88acab046e6edfb11323924e
JC Quirin <jcq@jcq.local>
2019-12-27 15:03:05 -0500

    temporarily disabled gatsby-plugin-purgecss: it is purging classes that we need in react-bootstrap components; there is a way to fix this, but that can come later;

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
     title: 'JC + Megan 2020',
@@ -65,13 +67,16 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
-    {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
-      options: {
-        develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.scss'], // applies purging only on the bulma css file
-      },
-    }, // must be after other CSS plugins
+    // {
+    //   resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
+    //   options: {
+    //     develop: true, // Activates purging in npm run develop
+    //     content: [
+    //       path.join(process.cwd(), 'node_modules/react-bootstrap/**/!(*.d).{ts,js,jsx,tsx}')
+    //     ]
+    //     // purgeOnly: ['/all.scss'], // applies purging only on our main css file
+    //   },
+    // }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
