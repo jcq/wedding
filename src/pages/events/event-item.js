2b45dd0f2bfda6bbf1568c557a0f0de87410b4d6
JC Quirin <jcq@jcq.local>
2019-12-28 15:13:46 -0500

    adjusted schema & queries to properly pull event_items

---
+++ src/pages/events/event-item.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const EventItem = ({ title, date, body }) => {
+  return (
+    <div>
+      <h4>{title}</h4>
+      <p>{date}</p>
+    </div>
+  );
+};
+
+EventItem.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.string,
+  description: PropTypes.string,
+  featuredevent: PropTypes.string,
+  featuredimage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  body: PropTypes.string
+};
--- src/templates/events-page.js
+++ src/templates/events-page.js
@@ -3,14 +3,27 @@ import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import Content, { HTMLContent } from '../components/Content';
+import { EventItem } from '../pages/events/event-item';
+import { Row, Col } from 'react-bootstrap';
 
-export const EventsPageTemplate = ({ title, content, contentComponent }) => {
+
+
+export const EventsPageTemplate = ({ title, content, contentComponent, event_items }) => {
   const PageContent = contentComponent || Content;
+
+  console.log('EventsPageTemplate', event_items)
 
   return (
     <section className="section">
       <h2 className="title">{title}</h2>
       <PageContent className="content" content={content} />
+      <Row>
+        {
+          event_items.map(({id, title, date, body}) => (
+            <Col key={id}><EventItem title={title} date={date} body={body} /></Col>
+          ))
+        }
+      </Row>
     </section>
   );
 };
@@ -18,7 +31,15 @@ export const EventsPageTemplate = ({ title, content, contentComponent }) => {
 EventsPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
-  contentComponent: PropTypes.func
+  contentComponent: PropTypes.func,
+  event_items: PropTypes.shape({
+    title: PropTypes.string,
+    date: PropTypes.string,
+    description: PropTypes.string,
+    featuredevent: PropTypes.string,
+    featuredimage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    body: PropTypes.string
+  })
 };
 
 const EventsPage = ({ data }) => {
@@ -30,6 +51,7 @@ const EventsPage = ({ data }) => {
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
+        event_items={post.frontmatter.event_items}
       />
     </Layout>
   );
@@ -47,6 +69,11 @@ export const eventsPageQuery = graphql`
       html
       frontmatter {
         title
+        event_items {
+          title
+          date(formatString: "MMMM DD, HH:mm")
+          description
+        }
       }
     }
   }
