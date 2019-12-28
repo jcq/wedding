f318493472e4bc0f6cee0f70658e777f0c611683
JC Quirin <jcq@jcq.local>
2019-12-28 11:57:15 -0500

    initial work on RSVP form

---
+++ src/pages/rsvp/rsvp-form.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+
+import { Form } from 'react-bootstrap';
+
+const handleSubmit = event => {
+  console.log('handleSubmit', event);
+
+  const payload = {};
+
+  event.preventDefault();
+};
+
+export const RsvpForm = () => {
+  const [attendees, setAttendees] = useState(0);
+  const [names, setNames] = useState([]);
+  const [notes, setNotes] = useState('');
+
+  useEffect(() => {
+    if (attendees > names.length) {
+      const toAdd = attendees - names.length;
+      setNames([...names, ...Array.from({ length: toAdd }, (v, i) => '')]);
+    } else if (attendees < names.length) {
+      const toRemove = names - attendees;
+      setNames([...names.splice(attendees - 1, toRemove)]);
+    }
+  }, [attendees]);
+
+  const handleChangeName = event => {
+    console.log('handleChangeName', event.target.dataset.idx);
+    const updatedNames = [...names];
+    updatedNames[event.target.dataset.idx] = event.target.value;
+    setNames(updatedNames);
+  };
+
+  return (
+    <Form name="rsvp" onSubmit={handleSubmit}>
+      <Form.Group controlId="howMany">
+        <Form.Label>How many will be attending?</Form.Label>
+        <Form.Control
+          type="number"
+          placeholder="Enter a number"
+          value={attendees}
+          onChange={event => setAttendees(event.target.value)}
+        ></Form.Control>
+      </Form.Group>
+      {names.map((name, idx) => (
+        <Form.Group controlId={'name-' + idx} key={'name-' + idx}>
+          <Form.Label>Name {idx + 1}</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter name"
+            data-idx={idx}
+            value={name}
+            onChange={handleChangeName}
+          ></Form.Control>
+        </Form.Group>
+      ))}
+      <Form.Group controlId="notes">
+        <Form.Label>Notes</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows="3"
+          placeholder="Is there anything else we should know?"
+          value={notes}
+          onChange={event => setNotes(event.target.value)}
+        ></Form.Control>
+      </Form.Group>
+    </Form>
+  );
+};
--- src/templates/rsvp-page.js
+++ src/templates/rsvp-page.js
@@ -1,28 +1,30 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import Content, { HTMLContent } from '../components/Content';
+import { RsvpForm } from '../pages/rsvp/rsvp-form';
 
 export const RsvpPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+  const PageContent = contentComponent || Content;
 
   return (
     <section className="section">
       <h2 className="title">{title}</h2>
       <PageContent className="content" content={content} />
+      <RsvpForm />
     </section>
-  )
-}
+  );
+};
 
 RsvpPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
-  contentComponent: PropTypes.func,
-}
+  contentComponent: PropTypes.func
+};
 
 const RsvpPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <Layout>
@@ -32,14 +34,14 @@ const RsvpPage = ({ data }) => {
         content={post.html}
       />
     </Layout>
-  )
-}
+  );
+};
 
 RsvpPage.propTypes = {
-  data: PropTypes.object.isRequired,
-}
+  data: PropTypes.object.isRequired
+};
 
-export default RsvpPage
+export default RsvpPage;
 
 export const rsvpPageQuery = graphql`
   query RsvpPage($id: String!) {
@@ -50,4 +52,4 @@ export const rsvpPageQuery = graphql`
       }
     }
   }
-`
+`;
