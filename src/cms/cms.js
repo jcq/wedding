e8c8760706cf9b0311f667e93b545f70de7c87c6
JC Quirin <jcq@jcq.lan>
2020-01-09 22:05:49 -0800

    added FAQ to CMS config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,6 +4,7 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import EventsPagePreview from './preview-templates/EventsPagePreview'
+import FaqPagePreview from './preview-templates/FaqPagePreview'
 import GetInvolvedPagePreview from './preview-templates/GetInvolvedPagePreview'
 import RsvpPagePreview from './preview-templates/RsvpPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
@@ -19,6 +20,7 @@ CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('faq', FaqPagePreview)
 CMS.registerPreviewTemplate('events', EventsPagePreview)
 CMS.registerPreviewTemplate('get-involved', GetInvolvedPagePreview)
 CMS.registerPreviewTemplate('rsvp', RsvpPagePreview)
