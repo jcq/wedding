b87f6d9220c63ea9d68265a73bdd78e252e93040
JC Quirin <jcq@jcq.lan>
2020-01-19 23:05:53 -0800

    removed unused product page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -8,7 +8,6 @@ import FaqPagePreview from './preview-templates/FaqPagePreview'
 import GetInvolvedPagePreview from './preview-templates/GetInvolvedPagePreview'
 import RsvpPagePreview from './preview-templates/RsvpPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 import styles from '!css-loader!sass-loader!../components/all.scss'
@@ -24,5 +23,4 @@ CMS.registerPreviewTemplate('faq', FaqPagePreview)
 CMS.registerPreviewTemplate('events', EventsPagePreview)
 CMS.registerPreviewTemplate('get-involved', GetInvolvedPagePreview)
 CMS.registerPreviewTemplate('rsvp', RsvpPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
