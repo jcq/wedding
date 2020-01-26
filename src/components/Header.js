a7a6f5170b10acc6aa9cca93e25364890c6648ab
JC Quirin <jcq@jcq.lan>
2020-01-25 19:29:44 -0800

    refactored Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,120 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import logo from '../img/logo.svg';
 import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 
-const NavButton = (props) => (
-  <Button as={Link} {...props} />
-)
+import headerStyles from './Header.scss';
 
-const Header = class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: false,
-      navBarActiveClass: ''
-    };
-  }
+const NavButton = props => <Button as={Link} {...props} />;
 
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: 'is-active'
-            })
-          : this.setState({
-              navBarActiveClass: ''
-            });
+const Header = ({ variant = 'light' }) => {
+  return (
+    <Navbar
+      expand="lg"
+      collapseOnSelect
+      variant={variant}
+      className={
+        variant === 'light' ? headerStyles.navbarLight : headerStyles.navbarDark
       }
-    );
-  };
+    >
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img src={logo} alt="Logo" style={{ width: '200px' }} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/about">
+              About Us
+            </Nav.Link>
+            <Nav.Link as={Link} to="/events">
+              Events
+            </Nav.Link>
+            <Nav.Link as={Link} to="/get-involved">
+              Get Involved
+            </Nav.Link>
+            <Nav.Link as={Link} to="/faq">
+              FAQ
+            </Nav.Link>
+            <NavButton to="/rsvp" variant="danger">
+              RSVP
+            </NavButton>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-  render() {
-    return (
-      <Navbar expand="lg" variant="dark">
-        <Container>
-          <Navbar.Brand as={Link} to="/">
-            <img src={logo} alt="Logo" style={{ width: '120px' }} />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link as={Link} to="/about">
-                About Us
-              </Nav.Link>
-              <Nav.Link as={Link} to="/events">
-                Events
-              </Nav.Link>
-              <Nav.Link as={Link} to="/get-involved">
-                Get Involved
-              </Nav.Link>
-              <Nav.Link as={Link} to="/faq">
-                FAQ
-              </Nav.Link>
-              <NavButton to="/rsvp" variant="danger">
-                RSVP
-              </NavButton>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      // <nav
-      //   className="navbar is-transparent"
-      //   role="navigation"
-      //   aria-label="main-navigation"
-      // >
-      //   <div className="container">
-      //     <div className="navbar-brand">
-      //       <Link to="/" className="navbar-item" title="Logo">
-      //         <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-      //       </Link>
-      //       {/* Hamburger menu */}
-      //       <div
-      //         className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-      //         data-target="navMenu"
-      //         onClick={() => this.toggleHamburger()}
-      //       >
-      //         <span />
-      //         <span />
-      //         <span />
-      //       </div>
-      //     </div>
-      //     <div
-      //       id="navMenu"
-      //       className={`navbar-menu ${this.state.navBarActiveClass}`}
-      //     >
-      //       <div className="navbar-start has-text-centered">
-      //         <Link className="navbar-item" to="/about">
-      //           About Us
-      //         </Link>
-      //         <Link className="navbar-item" to="/events">
-      //           Events
-      //         </Link>
-      //         <Link className="navbar-item" to="/get-involved">
-      //           Get Involved
-      //         </Link>
-      //         <Link className="navbar-item" to="/rsvp">
-      //           RSVP
-      //         </Link>
-      //         <Link className="navbar-item" to="/contact">
-      //           Contact
-      //         </Link>
-      //         <Link className="navbar-item" to="/contact/examples">
-      //           Form Examples
-      //         </Link>
-      //       </div>
-      //     </div>
-      //   </div>
-      // </nav>
-    );
-  }
+Header.propTypes = {
+  variant: PropTypes.string
 };
 
 export default Header;
