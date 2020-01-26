b8077b4eae26a3353bc3f87da5c8e19c8d444c1d
JC Quirin <jcq@jcq.lan>
2020-01-25 19:29:21 -0800

    implemented ImageHeader component

---
+++ src/components/ImageHeader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Jumbotron } from 'react-bootstrap';
+
+import styles from './ImageHeader.scss';
+
+export const ImageHeader = ({ image, children }) => {
+  const jumbotronStyles = {
+    backgroundImage: `url(${
+      !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+    })`
+  };
+
+  return (
+    <Jumbotron fluid style={jumbotronStyles} className={`${styles.jumbotron} d-flex align-items-center`}>
+      {children}
+    </Jumbotron>
+  );
+};
